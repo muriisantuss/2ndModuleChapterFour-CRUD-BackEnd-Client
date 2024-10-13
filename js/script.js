7f81395e60bc8324e4edc14dc8f1e09111f5a920
muriisantuss <muriisantuss@gmail.com>
2024-10-13 14:36:11 -0300

    :lipstick: closes #5 feat: Implements messageError on project

--- js/script.js
+++ js/script.js
@@ -1,22 +1,39 @@
-$("#cep").mask("99.999-999")
-
+$("#cep").mask("99.999-999");
 
 function validationCep() {
   var cep = document.getElementById("cep").value;
-  cep = cep.replace(/[.-]/g, "")
+  cep = cep.replace(/[.-]/g, "");
   let uri = `https://viacep.com.br/ws/${cep}/json/`;
 
   $.getJSON(uri, (set) => {
-    insertValueOnInput(set);
-  })
+    if (set.erro) {
+      showError("CEP não encontrado");
+    } else {
+      insertValueOnInput(set);
+    }
+  }).fail(() => {
+    showError("CEP inválido");
+  });
 }
 
 function insertValueOnInput(set) {
-  console.log(set);
+  showError("");
   document.getElementById("endereco").value = `${set.logradouro}`;
   document.getElementById("bairro").value = `${set.bairro}`;
   document.getElementById("cidade").value = `${set.localidade}`;
   document.getElementById("estado").value = `${set.estado}`;
-  document.getElementById("numero").removeAttribute("readonly")
+  document.getElementById("numero").removeAttribute("readonly");
 }
 
+function clearValueOnInput() {
+  document.getElementById("endereco").value = "";
+  document.getElementById("bairro").value = "";
+  document.getElementById("cidade").value = "";
+  document.getElementById("estado").value = "";
+  document.getElementById("numero").setAttribute("readonly", "readonly");
+}
+
+function showError(msg) {
+  clearValueOnInput();
+  document.getElementById("errorMessage").innerHTML = msg;
+}
